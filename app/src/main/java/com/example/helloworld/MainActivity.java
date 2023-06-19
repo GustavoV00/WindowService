c06ef0edabf643dc24179640f13f20fa205df1e5
Gustavo Valente <62510088+GustavoV00@users.noreply.github.com>
2023-06-19 16:39:21 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -31,13 +31,13 @@ public class MainActivity extends AppCompatActivity {
         if (resultado < 19) {
             textResultado.setText("Abaixo do peso: " + resultado);
 
-        } else if (resultado <= 19 || resultado < 25) {
+        } else if (resultado >= 19 || resultado < 25) {
             textResultado.setText("Peso normal: " + resultado);
 
-        } else if (resultado <= 25 || resultado < 30) {
+        } else if (resultado >= 25 || resultado < 30) {
             textResultado.setText("Sobrepeso: " + resultado);
 
-        } else if (resultado <= 30 || resultado < 40) {
+        } else if (resultado >= 30 || resultado < 40) {
             textResultado.setText("Obesidade tipo 1: " + resultado);
 
         } else if (resultado >= 40) {
