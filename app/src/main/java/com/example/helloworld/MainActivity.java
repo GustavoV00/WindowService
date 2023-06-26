32e1774a88e75ab0c6fd543dcbe303d1346a74c7
Gustavo Valente <62510088+GustavoV00@users.noreply.github.com>
2023-06-26 19:28:01 -0300

    Update MainActivity.java
    Remove older app implementation

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.helloworld;
 
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -9,42 +14,5 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textResultado;
-    private EditText editPeso, editAltura;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        textResultado = findViewById(R.id.textResultado);
-        editAltura = findViewById(R.id.editAltura);
-        editPeso = findViewById(R.id.editPeso);
-    }
-
-    public void calcular(View view) {
-
-        double peso = Double.parseDouble(editPeso.getText().toString());
-        double altura = Double.parseDouble(editAltura.getText().toString());
-        double resultado = peso / (altura * altura);
-
-        if (resultado < 19) {
-            textResultado.setText("Abaixo do peso: " + resultado);
-
-        } else if (resultado >= 19 || resultado < 25) {
-            textResultado.setText("Peso normal: " + resultado);
-
-        } else if (resultado >= 25 || resultado < 30) {
-            textResultado.setText("Sobrepeso: " + resultado);
-
-        } else if (resultado >= 30 || resultado < 40) {
-            textResultado.setText("Obesidade tipo 1: " + resultado);
-
-        } else if (resultado >= 40) {
-            textResultado.setText("Obesidade tipo 2: " + resultado);
-        }
-
-    }
-
-
 }
+
